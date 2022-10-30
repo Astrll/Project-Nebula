b67babe713c0b13ea4354577ed615d078cef6137
Astrll <116001038+Astrll@users.noreply.github.com>
2022-10-30 19:05:05 +0300

    Update script-loader.lua

--- script-loader.lua
+++ script-loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 2809202155 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrll/Project-Nebula/main/YBA/YBA.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrll/Project-Nebula/main/YBA/YBA.lua"))()
 end
